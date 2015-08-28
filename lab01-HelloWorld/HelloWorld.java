ca932a3de6db131388b702dbd8f50b538ab7a9da
guj215 <guj215@lehigh.edu>
2015-08-28 15:54:56 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,28 @@
+//Guilherme Junqueira Perticarari
+//Lab 1 Program
+
+//Defining the HelloWorld class
+public class HelloWorld{
+    
+    //Adding the main method
+    public static void main(String[] args){
+        
+        //Printing the statement inside a box
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#    Hello World!      #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("testing");
+
+        
+        
+    }
+    
+    
+    
+    
+    
+}
