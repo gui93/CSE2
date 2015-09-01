35c5c678d9ca0fe2002276ed5ce490312fda4ff6
guj215 <guj215@lehigh.edu>
2015-09-01 14:24:53 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+//Guilherme Junqueira Perticarari 824858244
+//Homework 1 - CSE2
+
+
+//Creation of the WelcomeClass class
+public class WelcomeClass{
+
+    //Creation of the main method
+    public static void main(String[] args){
+    
+        //Printing out the first three lines
+        System.out.println("    -----------");
+        System.out.println("    | WELCOME |");
+        System.out.println("    -----------");
+       
+     
+        //Printing out the remaining lines
+        System.out.println("   ^  ^  ^  ^  ^  ^ ");
+        System.out.println("  / \\/ \\/ \\/ \\/ \\/ \\   ");        
+        //Printing out my Lehigh Network ID
+        System.out.println(" <-g--u--j--2--1--5-> ");
+        System.out.println("  \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("   v  v  v  v  v  v ");
+        System.out.println("Today I had ice-cream with two of my international friends. One is from England and the other one, from Germany.");
+
+
+    
+    
+    
+        }
+    }
